439c792ecd6fe1eaf40e57a8e3a2063c4d5344cc
arianaomi <arianaomi.lp@gmail.com>
2022-05-29 23:06:42 -0500

    prisma

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
+import { UserType } from '@prisma/client';
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup(){
-    
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email, password, name, phone }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userExists) {
+      throw new ConflictException();
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log({ hashedPassword });
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        phone,
+        name,
+        password: hashedPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    //generate the token
+    const token = await jwt.sign(
+      {
+        name,
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: 3600 },
+    );
+    return {user, token};
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController, AuthController],
-  providers: [UserService, AuthService]
+  providers: [UserService, AuthService],
 })
 export class UserModule {}
